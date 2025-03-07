ed086b6b103fec6fb3feb5fe55148638dd4faf4b
Nassim <aa.nassim.ddr@gmail.com>
2025-03-07 22:53:53 +0100

    Experience animation plus responsive design

--- src/components/CanvasContainer.jsx
+++ src/components/CanvasContainer.jsx
@@ -96,7 +96,6 @@ const CanvasContainer = () => {
       z: 5,
       y: 5, 
       x: 10, 
-      immediateRender: false, 
     }, {
       z: 9.8,
       y: 4.6,
@@ -104,22 +103,20 @@ const CanvasContainer = () => {
       immediateRender: false, 
       scrollTrigger: {
         trigger: ".animation-experience-to-exp-1", 
-        start: "top bottom", 
+        start: "top 50%", 
         end: "top top",
         scrub: 0.5,
-        markers: true, 
       }
     })
 
     gsap.fromTo(camera.rotation, {
       y: Math.PI,
-      immediateRender: false, 
     },  {
       y: 0.8 *  Math.PI,
       immediateRender: false, 
       scrollTrigger: {
         trigger: ".animation-experience-to-exp-1", 
-        start: "top bottom", 
+        start: "top 50%", 
         end: "top top",
         scrub: 0.5,
       }
@@ -130,7 +127,6 @@ const CanvasContainer = () => {
       x: 10.6,
       y: 4.6,
       z: 9.8,
-      immediateRender: false, 
     }, {
       x: 8.4, 
       y: 5.2,
@@ -146,7 +142,6 @@ const CanvasContainer = () => {
 
     gsap.fromTo(camera.rotation, {
       y: 0.8 *  Math.PI,
-      immediateRender: false, 
     }, {
       y: 1.3 * Math.PI, 
       // x: -0.1 * Math.PI, 
@@ -175,157 +170,12 @@ const CanvasContainer = () => {
         start: "top bottom", 
         end: "top top", 
         scrub: 0.5, 
-        markers: true, 
       }
     })
 
     camera.position.set(0, 5, -10)
     camera.rotation.set(0, 0, 0)
   }, [])
-
-  // useGSAP(() => {
-  //   let cameraPositionTL = gsap.timeline({
-  //     immediateRender: false,
-  //     scrollTrigger: {
-  //       trigger: ".second-section", 
-  //       endTrigger: ".third-section", 
-  //       start: "top bottom", 
-  //       end: "top top", 
-  //       scrub: true, 
-  //       // snap: 0.5
-  //     }
-  //   })
-
-  //   let cameraRotationTL = gsap.timeline({
-  //     immediateRender: false,
-  //     scrollTrigger: {
-  //       trigger: ".third-section", 
-  //       endTrigger: ".fourth-section", 
-  //       start: "top bottom", 
-  //       end: "top top", 
-  //       scrub: 0.5, 
-  //     }
-  //   })
-
-  //   cameraPositionTL.to(camera.position, {
-  //     z:5
-  //   }).to(camera.position, {
-  //     x: 10, 
-  //   })
-    
-  //   // gsap.fromTo(camera.position, {
-  //   //   x: 0, 
-  //   //   y: 5, 
-  //   //   z: -10
-  //   // }, {
-  //   //   x: 0, 
-  //   //   y: 5, 
-  //   //   z: 5, 
-  //   //   immediateRender: false, 
-  //   //   scrollTrigger: {
-  //   //     trigger: "aniimation-hero-to-about", 
-  //   //     start: "top bottom", 
-  //   //     end: "top top", 
-  //   //     scrub: 0.5, 
-  //   //   }
-  //   // })
-
-
-  //   cameraRotationTL.to(camera.rotation, {
-  //     y: Math.PI / 2, 
-  //   }).to(camera.rotation, {
-  //     y: Math.PI
-  //   })
-
-  //   // Transition to experience 1: 
-  //   gsap.fromTo(camera.position, {
-  //     z: 5,
-  //     y: 5, 
-  //     x: 10, 
-  //     immediateRender: false, 
-  //   }, {
-  //     z: 9.8,
-  //     y: 4.6,
-  //     x: 10.6,
-  //     immediateRender: false, 
-  //     scrollTrigger: {
-  //       trigger: ".experience-animation", 
-  //       start: "40% 30%", 
-  //       end: "bottom top",
-  //       scrub: 0.5,
-  //     }
-  //   })
-
-  //   gsap.fromTo(camera.rotation, {
-  //     y: Math.PI,
-  //     immediateRender: false, 
-  //   },  {
-  //     y: 0.8 *  Math.PI,
-  //     immediateRender: false, 
-  //     scrollTrigger: {
-  //       trigger: ".experience-animation", 
-  //       start: "80% 30%", 
-  //       end: "bottom top",
-  //       scrub: 0.5,
-  //     }
-  //   })
-
-  //   // transition to expeirence 2
-  //   gsap.fromTo(camera.position, {
-  //     x: 10.6,
-  //     y: 4.6,
-  //     z: 9.8,
-  //     immediateRender: false, 
-  //   }, {
-  //     x: 8.4, 
-  //     y: 5.2,
-  //     z: 11.4, 
-  //     immediateRender: false, 
-  //     scrollTrigger: {
-  //       trigger: ".first-experience", 
-  //       start: "60% top", 
-  //       end: "bottom top", 
-  //       scrub: 0.5, 
-  //     }
-  //   })
-
-  //   gsap.fromTo(camera.rotation, {
-  //     y: 0.8 *  Math.PI,
-  //     immediateRender: false, 
-  //   }, {
-  //     y: 1.3 * Math.PI, 
-  //     // x: -0.1 * Math.PI, 
-  //     immediateRender: false, 
-  //     scrollTrigger: {
-  //       trigger: ".first-experience", 
-  //       start: "70% top", 
-  //       end: "bottom top", 
-  //       scrub: 0.5, 
-  //     }, 
-  //   }
-  //   )
-
-  //   gsap.fromTo(camera.position, {
-  //     x: 8.4, 
-  //     y: 5.2,
-  //     z: 11.4, 
-  //   }, {
-  //     immediateRender: false, 
-  //     x: 10.45, 
-  //     y: 5.7, 
-  //     z: 13.065,
-  //     scrollTrigger: {
-  //       trigger: ".second-experience", 
-  //       start: "70% top", 
-  //       end: "bottom top", 
-  //       scrub: 0.5, 
-  //       // markers: true, 
-  //     }
-  //   })
-
-  //   camera.position.set(0, 5, -10)
-  //   camera.rotation.set(0, 0, 0)
-  // }, [])
 
   return ( 
     <div className='w-full h-full fixed top-0 left-0 -z-10'>
--- src/sections/Experiences.jsx
+++ src/sections/Experiences.jsx
@@ -4,10 +4,48 @@ import React, { useRef } from 'react'
 
 
 const ExperienceContent = (props) => {
+  const ExperiencePanelRef = useRef()
+
+  useGSAP(() => {
+    gsap.fromTo(ExperiencePanelRef.current, {
+      width: "0%", 
+      height: "0%",
+      opacity: 0, 
+    }, {
+      width: "100%",
+      height: "50%",
+      opacity: 1, 
+
+      duration: 0.5, 
+      ease: "power4.out",
+
+      scrollTrigger: {
+        trigger: ExperiencePanelRef.current, 
+        // pin: PanelParenRef.current,
+        start: "top 35%",
+        end : "top 10%", 
+        toggleActions: "restart reverse restart reverse", 
+        markers: true, 
+      }
+    })
+  }, [])
+
   return (
-    <div className={'h-screen w-full c-space flex items-center justify-center' + ' ' + props.addClass}>
-      <div className='h-1/2 w-full bg-blue-600 '>
-        Project 1
+    <div className='h-screen w-full c-space flex justify-center experience-content'>
+      <div ref={ExperiencePanelRef} className='h-[100%] sm:h-[50%] w-full flex flex-col sm:flex-row bg-blue-600 rounded-[1rem] overflow-hidden'>
+        <div className='h-full w-full sm:w-[50%] bg-red-600  p-5'>
+          <p className='text-left xl:text-4xl md:text-3xl sm:text-2xl text-xl font-generalsans font-black !leading-normal text-white'> 
+            JAMBOUR
+          </p>
+          <p className='text-left xl:text-2xl md:text-xl sm:text-l text-m font-generalsans !leading-normal text-white'>
+            JAMBOURJBOURQSD SQBD QISB DYQBS DYHQSDHSHQDQSDUO QHSDUHQSDUH QSUDUQHSD QHD QDSQ
+          </p>
+        </div>
+
+        <div className='h-full w-full sm:w-[50%] bg-yellow-600'>
+
+        </div>
+
       </div>
     </div>
   )
@@ -43,14 +81,13 @@ const Experiences = () => {
     <section className='w-screen fourth-section text-white-800'>
       <div className='h-screen animation-experience-to-exp-1'>
       </div>
-      <ExperienceContent addClass='first-experience' textContent=""/> 
+      <ExperienceContent /> 
       <div className='h-screen animation-exp-1-to-exp-2'>
       </div>
-      <ExperienceContent addClass='second-experience'/> 
+      <ExperienceContent /> 
       <div ref={transitionDivRef} className='h-screen animation-exp-2-to-exp-3'>
       </div>
-      {/* <ThirdExperience />  */}
-      <ExperienceContent  addClass='third-experience' />
+      <ExperienceContent />
     </section>
   )
 }
