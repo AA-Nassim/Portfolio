be7f5a5b7cadb12ce4cd4666c21381dd2a9658c8
Nassim <aa.nassim.ddr@gmail.com>
2025-03-12 18:29:11 +0100

    Added video demos

--- src/sections/Experiences.jsx
+++ src/sections/Experiences.jsx
@@ -31,19 +31,21 @@ const ExperienceContent = (props) => {
   }, [])
 
   return (
-    <div className='h-screen w-full c-space flex justify-center experience-content'>
-      <div ref={ExperiencePanelRef} className='h-[100%] sm:h-[50%] w-full flex flex-col sm:flex-row bg-blue-600 rounded-[1rem] overflow-hidden'>
-        <div className='h-full w-full sm:w-[50%] bg-red-600  p-5'>
-          <p className='text-left xl:text-4xl md:text-3xl sm:text-2xl text-xl font-generalsans font-black !leading-normal text-white'> 
+    <div className='h-screen w-full c-space experience-content flex justify-center'>
+      <div ref={ExperiencePanelRef} className='flex flex-col md:flex-row justify-center rounded-[1rem] overflow-hidden'>
+        <div className='h-full md:w-[40%] bg-red-600  p-5'>
+         <p className='text-left xl:text-4xl md:text-3xl sm:text-2xl text-xl font-generalsans font-black !leading-normal text-white'> 
             JAMBOUR
           </p>
           <p className='text-left xl:text-2xl md:text-xl sm:text-l text-m font-generalsans !leading-normal text-white'>
             JAMBOURJBOURQSD SQBD QISB DYQBS DYHQSDHSHQDQSDUO QHSDUHQSDUH QSUDUQHSD QHD QDSQ
-          </p>
+          </p>  
         </div>
 
-        <div className='h-full w-full sm:w-[50%] bg-yellow-600'>
-
+        <div className='h-auto  md:h-full w-auto bg-yellow-600'>
+        <video  autoPlay loop muted className='h-[100%] w-full'>
+          <source src='./assets/Demo_CodeDelta.mp4' type='video/mp4'/>
+        </video>
         </div>
 
       </div>
