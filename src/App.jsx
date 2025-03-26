252286800b5959446e390b595c139d7b1800442e
Nassim <aa.nassim.ddr@gmail.com>
2025-03-26 13:06:56 +0100

    Cnavas loader update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 'use client'
 
-import React, { useEffect } from 'react';
+import React, {Suspense} from 'react';
 import Navbar from './sections/Navbar';
 import Hero from './sections/hero';
 import Footer from './sections/Foot';
 import About from './sections/About';
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-
+import CanvasLoader from './components/CanvasLoader';
 import ReactLenis from 'lenis/react';
 import Skills from './sections/Skills';
 import Experiences from './sections/Experiences';
@@ -21,19 +21,22 @@ const App = () => {
   return ( 
   <ReactLenis root>
     <main className='mx-auto section-container'>
-      <Navbar />
-      <CanvasContainer />
-      <Hero /> 
-      <div className='h-[50vh] animation-hero-to-about'>
-      </div>
-      <About />
-      <div className='h-screen animation-about-to-skills'>
-      </div>
-      <Skills /> 
-      <div className='h-[50vh] animation-skills-to-work'>
-      </div>
-      <Experiences /> 
-      <Footer />
+      <Suspense fallback={<CanvasLoader />}>
+        <Navbar />
+        <CanvasContainer />
+        <Hero /> 
+        <div className='h-[50vh] animation-hero-to-about'>
+        </div>
+        <About />
+        <div className='h-screen animation-about-to-skills'>
+        </div>
+        <Skills /> 
+        <div className='h-[50vh] animation-skills-to-work'>
+        </div>
+        <Experiences /> 
+        <Footer />
+      </Suspense>                
+      
     </main>
   </ReactLenis>
   );
